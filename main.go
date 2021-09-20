bb4d902a453dd466bc3d85c7fe1d0f864b405215
Brittney Coble <brittneycoble@theartofeducation.edu>
2021-09-20 16:32:08 -0400

    function returning type error with log message; uses imported errors package

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 // import block
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 /*
 gRPC API: Attempt 1; On Mac
@@ -12,12 +15,15 @@ func main() {
 	//create port variable and set port on which to listen
 	port := 3000
 
-	// pass in port variable as teh function argument
-	startWebServer(port, 2)
+	// pass in port variable as the function argument
+	// bool returned from function whether server started or not; printed here as err if error is returned
+	err := startWebServer(port, 2)
+	fmt.Println(err)
+
 }
 
 // declares variable and type for port as function argument
-func startWebServer(port int, numberOfRetries int) {
+func startWebServer(port int, numberOfRetries int) error {
 	fmt.Println(("Starting server..."))
 	// do server stuff
 	// print text with port that is being listened
@@ -25,4 +31,5 @@ func startWebServer(port int, numberOfRetries int) {
 
 	//number of retries if port doesn't start on first try
 	fmt.Println("Number of retries", numberOfRetries)
+	return errors.New("Something went wrong.")
 }
