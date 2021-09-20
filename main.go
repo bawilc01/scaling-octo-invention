a27fee1b21a12c7ecd349301970a42a546a34532
Brittney Coble <brittneycoble@theartofeducation.edu>
2021-09-20 15:56:20 -0400

    setup function; ran web server function

--- main.go
+++ main.go
@@ -8,5 +8,12 @@ gRPC API: Attempt 1; On Mac
 Helpful links: https://grpc.io/docs/languages/go/basics/
 */
 func main() {
-	fmt.Println("!... Hello World ...!")
+	fmt.Println("Hello there.")
+	startWebServer()
+}
+
+func startWebServer() {
+	fmt.Println(("Starting server..."))
+	// do server stuff
+	fmt.Println("Server started")
 }
